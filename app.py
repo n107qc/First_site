e6f829c0e1c2ca6d52281b95dc6e7fb3dbf7c20e
Бублик НАзарій <nazariy.bublyk09@gmail.com>
2024-11-16 12:31:45 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+from db_scripts import DatabaseManager
+
+app = Flask(__name__)  # Створюємо веб–додаток Flask
+db = DatabaseManager("blog.db")
+
+
+@app.route("/")  # Вказуємо url-адресу для виклику функції
+def index():
+    articles = db.get_all_articles()
+    
+    return render_template("index.html",  articles=articles)  # html-сторінка, що повертається у браузер
+
+
+if __name__ == "__main__":
+    app.config['TEMPLATES_AUTO_RELOAD'] = True  # автоматичне оновлення шаблонів
+    app.run(debug=True)  # Запускаємо веб-сервер з цього файлу в режимі налагодження   

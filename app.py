3bab8da6c455ab3fd7b9591d020ef80745218d2a
Бублик НАзарій <nazariy.bublyk09@gmail.com>
2024-12-07 12:30:32 +0200

    Lesosn 6

--- app.py
+++ app.py
@@ -5,12 +5,28 @@ from db_scripts import DatabaseManager
 app = Flask(__name__)  # Створюємо веб–додаток Flask
 db = DatabaseManager("blog.db")
 
+@app.context_processor
+def get_categories():
+    categories = db.get_all_categories()
+    return dict(categories=categories)
 
 @app.route("/")  # Вказуємо url-адресу для виклику функції
 def index():
     articles = db.get_all_articles()
-    
     return render_template("index.html",  articles=articles)  # html-сторінка, що повертається у браузер
+
+@app.route("/articles/<int:article_id>")  # Вказуємо url-адресу для виклику функції
+def article_page(article_id):
+    article = db.get_article(article_id)
+    return render_template("article_page.html",  article=article)  # html-сторінка, що повертається у браузер
+
+
+
+@app.route("/categories/<int:category_id>")  # Вказуємо url-адресу для виклику функції
+def category_page(category_id):
+    articles = db.get_categories_articles(category_id)
+    return render_template("index.html", articles=articles)  # html-сторінка, що повертається у браузер
+
 
 
 if __name__ == "__main__":
--- db_scripts.py
+++ db_scripts.py
@@ -2,11 +2,46 @@ import sqlite3
 
 class DatabaseManager:
     def __init__(self,dbname,):
-        self.conn = sqlite3.connect(dbname)
+        self.conn = None
+        self.cursor = None
+        self.dbname = dbname
+
+
+    def open(self):
+        self.conn = sqlite3.connect(self.dbname)
         self.cursor = self.conn.cursor()
 
 
+    def close(self):
+        self.cursor.close()
+        self.conn.close()
+
+
     def get_all_articles(self):
+        self.open()
         self.cursor.execute("""SELECT * FROM articles""")
         data = self.cursor.fetchall()
+        self.close()
+        return data
+    
+    def get_article(self,article_id):
+        self.open()
+        self.cursor.execute("""SELECT * FROM articles WHERE id=?""",[article_id])
+        data = self.cursor.fetchone()
+        self.close()
+        return data
+    
+    def get_all_categories(self):
+        self.open()
+        self.cursor.execute("""SELECT * FROM categories""")
+        data = self.cursor.fetchall()
+        self.close()
+        return data
+    
+
+    def get_categories_articles(self,category_id):
+        self.open()
+        self.cursor.execute("""SELECT * FROM articles WHERE category_id=?""",[category_id])
+        data = self.cursor.fetchall()
+        self.close()
         return data

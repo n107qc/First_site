5a50a59365e2f438e7dd8a7e89beabc9cd4c4672
Бублик НАзарій <nazariy.bublyk09@gmail.com>
2024-12-21 12:36:17 +0200

    додавання постів

--- db_scripts.py
+++ db_scripts.py
@@ -45,3 +45,12 @@ class DatabaseManager:
         data = self.cursor.fetchall()
         self.close()
         return data
+    
+
+    def add_article(self,title,content,image,used_id,category_id):
+        self.open()
+        self.cursor.execute("""INSERT INTO articles(title,content,image,user_id,category_id)
+                            VALUES(?,?,?,?,?)""",[title,content,image,used_id,int(category_id)])
+        self.conn.commit()
+        self.close()
+        return 
